b65e22e1806e0776dc7173d01fad0249022c83fb
michaeldaranto <36653385+michaeldaranto@users.noreply.github.com>
2022-04-20 17:45:11 +0700

    Add files via upload

--- projects/blink/src/main.c
+++ projects/blink/src/main.c
@@ -1,24 +1,29 @@
+/*********************************************************
+WeAct BlackPill STM32F411CEU6
+GPIOC Pin13 --> LED
+Blink LED 
+Michael Daranto - YD3BRB
+**********************************************************/
 #include "stm32f4xx.h"
 
-// Quick and dirty delay
-static void delay (unsigned int time) {
-    for (unsigned int i = 0; i < time; i++)
-        for (volatile unsigned int j = 0; j < 2000; j++);
+//Simple delay
+void delay (void){
+	for (uint32_t i=0; i < 500000; i++)
+		__asm("nop");
 }
-
 int main (void) {
     // Turn on the GPIOC peripheral
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     
-    // Set GPIOC Pin 13 Output
+    // Set GPIOC Pin13 Output
     GPIOC->MODER|= GPIO_MODER_MODE13_0;
     GPIOC->MODER&=~GPIO_MODER_MODE13_1;
 
     while (1) {
-	GPIOC->ODR ^=GPIO_ODR_OD13; //(1<<13);
-        delay(140);
+	//XOR to toggle
+    	GPIOC->ODR ^=GPIO_ODR_OD13; 
+    	delay();
     }
-
     // Return 0 to satisfy compiler
     return 0;
 }
