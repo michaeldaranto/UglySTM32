97fa29bbd5c0068a3208e7c78a9a862be31792bb
michaeldaranto <36653385+michaeldaranto@users.noreply.github.com>
2022-04-21 11:23:27 +0700

    Add files via upload

--- projects/blink/src/main.c
+++ projects/blink/src/main.c
@@ -3,6 +3,7 @@ WeAct BlackPill STM32F411CEU6
 GPIOC Pin13 --> LED
 Blink LED 
 Michael Daranto - YD3BRB
+GPLv3
 **********************************************************/
 #include "stm32f4xx.h"
 
@@ -12,18 +13,18 @@ void delay (void){
 		__asm("nop");
 }
 int main (void) {
-    // Turn on the GPIOC peripheral
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	// Turn on the GPIOC peripheral
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     
-    // Set GPIOC Pin13 Output
-    GPIOC->MODER|= GPIO_MODER_MODE13_0;
-    GPIOC->MODER&=~GPIO_MODER_MODE13_1;
+	// Set GPIOC Pin13 Output
+	GPIOC->MODER|= GPIO_MODER_MODE13_0;
+	GPIOC->MODER&=~GPIO_MODER_MODE13_1;
 
-    while (1) {
-	//XOR to toggle
-    	GPIOC->ODR ^=GPIO_ODR_OD13; 
-    	delay();
-    }
-    // Return 0 to satisfy compiler
-    return 0;
+	while (1) {
+	    //XOR to toggle
+    	    GPIOC->ODR ^=GPIO_ODR_OD13; 
+    	    delay();
+	}
+	// Return 0 to satisfy compiler
+	return 0;
 }
--- projects/button/src/main.c
+++ projects/button/src/main.c
@@ -4,6 +4,7 @@ GPIOA Pin0  --> Switch/Button
 GPIOC Pin13 --> LED
 Press the Button to slowdown (Blink LED) 
 Michael Daranto - YD3BRB
+GPLv3
 **********************************************************/
 
 #include "stm32f4xx.h"
@@ -14,31 +15,30 @@ void delay (void){
 		__asm("nop");
 }
 int main (void) {
-    // Turn on the GPIOC & GPIOA peripheral
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	// Turn on the GPIOC & GPIOA peripheral
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     
-    // GPIOA Pin0 input
-    GPIOA->MODER&=~GPIO_MODER_MODE0; 	
+	// GPIOA Pin0 input
+	GPIOA->MODER&=~GPIO_MODER_MODE0; 	
     
-    // GPIOA Pin13 output
-    GPIOC->MODER|= GPIO_MODER_MODE13_0; 
-    GPIOC->MODER&=~GPIO_MODER_MODE13_1;
+	// GPIOA Pin13 output
+	GPIOC->MODER|= GPIO_MODER_MODE13_0; 
+	GPIOC->MODER&=~GPIO_MODER_MODE13_1;
     
+	while (1) {
+    	    // Check if someone press the button 
+		if (GPIOA->IDR&GPIO_IDR_ID0) {
+		GPIOC->ODR ^=GPIO_ODR_OD13;
+		delay();
+		}
+		else {
+		GPIOC->ODR ^=GPIO_ODR_OD13;
+    		delay();
+		delay();
+    		}
+		}
 
-    while (1) {
-    	// Check if someone press the button 
-	if (GPIOA->IDR&GPIO_IDR_ID0) {
-	GPIOC->ODR ^=GPIO_ODR_OD13;
-	delay();
-	}
-	else {
-	GPIOC->ODR ^=GPIO_ODR_OD13;
-        delay();
-	delay();
-        }
-    }
-
-    // Return 0 to satisfy compiler
-    return 0;
+	// Return 0 to satisfy compiler
+	return 0;
 }
